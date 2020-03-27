3ea7f88ec150bce34c3df942adeeeba0f8a353a9
Tobias Pott <tobias@vrtxlabs.com>
2020-03-27 13:06:45 +0100

    Reorganised code and extracted blz service and calculator service to separate sample code files

---
+++ Assets/Scripts/Samples/BLZService.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Xml.Serialization;
+
+namespace VRTX.Net.Samples.BLZService
+{
+    public class BLZServiceSoapClient : SoapClient
+    {
+        // soap client implementation for BLZService (public sample wsdl service)
+        // API-URL:     http://www.thomas-bayer.com/axis2/services/BLZService
+        // Namespace:   http://thomas-bayer.com/blz/
+        //  List of available commands/endpoints
+        //              getBank(string blz)
+        public BLZServiceSoapClient() : base("http://www.thomas-bayer.com/axis2/services/BLZService", "http://thomas-bayer.com/blz/")
+        { }
+    }
+    public class GetBank : SoapOperation<GetBankRequest, GetBankResponse, GetBank>
+    {
+        public GetBank() : base("getBank") { }
+    }
+
+    /// <summary>
+    /// sample implementation of complexType 'detailsType' for BLZService wsdl service
+    /// </summary>
+    [XmlRoot(ElementName = "details", Namespace = "http://thomas-bayer.com/blz/")]
+    public class GetBankResponse : SoapResponseType
+    {
+        // optional member fields mapped using XmlElement and other Xml..-attributes
+        //  ->  they are deserialized using the SoapResponse.Deserialize method
+        [XmlElement("bezeichnung")]
+        public string Bezeichnung { get; set; }
+        [XmlElement("bic")]
+        public string BIC { get; set; }
+        [XmlElement("ort")]
+        public string Ort { get; set; }
+        [XmlElement("plz")]
+        public string PLZ { get; set; }
+    }
+    [XmlRoot(ElementName = "getBank", Namespace = "http://thomas-bayer.com/blz/")]
+    public class GetBankRequest : SoapRequestType
+    {
+        [XmlElement("blz")]
+        public string BLZ { get; set; }
+    }
+}
+
---
+++ Assets/Scripts/Samples/SoapClientSamples.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UnityEngine;
+
+namespace VRTX.Net.Samples
+{
+    public class SoapClientSamples : MonoBehaviour
+    {
+        [SerializeField()]
+        private TMPro.TextMeshProUGUI _results = null;
+        [SerializeField()]
+        private TMPro.TMP_InputField _input = null;
+
+        // service url:
+        // http://www.thomas-bayer.com/axis2/services/BLZService?wsdl
+        // service base url:
+        // http://www.thomas-bayer.com/axis2/services/BLZService
+        // service namespace:
+        // http://thomas-bayer.com/blz/
+
+        private BLZService.BLZServiceSoapClient _blzServiceClient = new BLZService.BLZServiceSoapClient();
+        private Calculator.CalculatorSoapClient _calculatorClient = new Calculator.CalculatorSoapClient();
+        // Start is called before the first frame update
+        public async void ExecuteBLZRequest()
+        {
+            BLZService.GetBankRequest request = new BLZService.GetBankRequest()
+            { BLZ = _input.text };
+
+            // concise way of sending request using the SoapOperation implementation
+            BLZService.GetBankResponse response = await BLZService.GetBank.Execute(_blzServiceClient, request);
+            if (response != null)
+            {
+                _results.text = "Result:" + Environment.NewLine;
+                _results.text += response.Bezeichnung.ToString() + Environment.NewLine;
+                _results.text += response.BIC.ToString() + Environment.NewLine;
+                _results.text += response.PLZ.ToString() + " " + response.Ort.ToString();
+            }
+        }
+
+        public async void ExecuteCalculatorAddRequest()
+        {
+            Calculator.AddRequest request = new Calculator.AddRequest()
+            {
+                intA = UnityEngine.Random.Range(-byte.MaxValue, byte.MaxValue),
+                intB = UnityEngine.Random.Range(-byte.MaxValue, byte.MaxValue)
+            };
+            Calculator.AddResponse response = await Calculator.Add.Execute(_calculatorClient, request);
+            if (response != null)
+                _results.text = string.Format("{0} + {1} = {2}", request.intA, request.intB, response.Value) + Environment.NewLine;
+        }
+        public async void ExecuteCalculatorSubtractRequest()
+        {
+            Calculator.SubtractRequest request = new Calculator.SubtractRequest()
+            {
+                intA = UnityEngine.Random.Range(-byte.MaxValue, byte.MaxValue),
+                intB = UnityEngine.Random.Range(-byte.MaxValue, byte.MaxValue)
+            };
+            Calculator.SubtractResponse response = await Calculator.Subtract.Execute(_calculatorClient, request);
+            if (response != null)
+                _results.text = string.Format("{0} - {1} = {2}", request.intA, request.intB, response.Value) + Environment.NewLine;
+        }
+        public async void ExecuteCalculatorMultiplyRequest()
+        {
+            Calculator.MultiplyRequest request = new Calculator.MultiplyRequest()
+            {
+                intA = UnityEngine.Random.Range(-byte.MaxValue, byte.MaxValue),
+                intB = UnityEngine.Random.Range(-byte.MaxValue, byte.MaxValue)
+            };
+            Calculator.MultiplyResponse response = await Calculator.Multiply.Execute(_calculatorClient, request);
+            if (response != null)
+                _results.text = string.Format("{0} * {1} = {2}", request.intA, request.intB, response.Value) + Environment.NewLine;
+        }
+        public async void ExecuteCalculatorDivideRequest()
+        {
+            Calculator.DivideRequest request = new Calculator.DivideRequest()
+            {
+                intA = UnityEngine.Random.Range(-byte.MaxValue, byte.MaxValue),
+                intB = UnityEngine.Random.Range(1, byte.MaxValue / 16)
+            };
+            Calculator.DivideResponse response = await Calculator.Divide.Execute(_calculatorClient, request);
+            if (response != null)
+                _results.text = string.Format("{0} / {1} = {2}", request.intA, request.intB, response.Value) + Environment.NewLine;
+        }
+
+    }
+
+}
